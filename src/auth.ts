86004d1812d0c8a90c60a749dcb3d0ff1bb2dddf
Jackson-Greene <Jackson.Greene@student.curtin.edu.au>
2022-09-16 23:11:02 +0800

    debug getSession

--- src/auth.ts
+++ src/auth.ts
@@ -221,17 +221,18 @@ export class Auth {
   };
 
   getSession: GetSession = async (event: RequestEvent) => {
-    const { request } = event;
-    const token = await this.getToken(request.headers);
+    console.log(event);
+  //   const { request } = event;
+  //   const token = await this.getToken(request.headers);
 
-    if (token) {
-      if (this.config?.callbacks?.session) {
-        return await this.config.callbacks.session(token, { user: token.user });
-      }
+  //   if (token) {
+  //     if (this.config?.callbacks?.session) {
+  //       return await this.config.callbacks.session(token, { user: token.user });
+  //     }
 
-      return { user: token.user };
-    }
+  //     return { user: token.user };
+  //   }
 
-    return {};
-  };
+  //   return {};
+  // };
 }
